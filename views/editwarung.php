41da6b8fccd74a9313fbbb47e11e2ee3c7549a6d
aldywijayag <aldywijayag@gmail.com>
2023-01-28 09:47:34 +0700

    test commit

--- views/editwarung.php
+++ views/editwarung.php
@@ -8,7 +8,6 @@
     <title>Edit Warung</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
 </head>
 
 <body>
@@ -17,7 +16,7 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="#" style="margin-left: 50px;">
                 <img src="../assets/alliance.png" alt="Logo" width="25">
-                Aldy Wijaya Gustian (19552011351)
+                Ulala Creep (19552011351)
             </a>
 
             <!-- Collapse Navbar-->
